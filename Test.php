c408e9dd55a427d084473fe8160957a38f70c590
hmurotani <hmurotani@gmail.com>
2018-10-13 17:46:00 +0900

    add..

---
+++ Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmurotani
+ * Date: 2018/10/10
+ * Time: 18:27
+ */
+
+
+class Test extends PHPUnit_Framework_TestCase
+{
+
+}
---
+++ zzz.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmurotani
+ * Date: 2018/10/10
+ * Time: 18:00
+ */
+
+echo "Good evening!";
+
+
+$lists = ['cat', 'dog', 'tiger'];
+foreach ($lists as $animal) {
+    echo "$animal\n";
+}
